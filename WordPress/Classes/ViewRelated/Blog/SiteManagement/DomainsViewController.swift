b25f834be41037042c09a1aab125f3ccbddfa283
Alex Curylo <alex@alexcurylo.com>
2016-03-07 18:57:40 -0800

    Add New Domains section structure

--- WordPress/Classes/ViewRelated/Blog/SiteManagement/DomainsViewController.swift
+++ WordPress/Classes/ViewRelated/Blog/SiteManagement/DomainsViewController.swift
@@ -10,7 +10,11 @@ public class DomainsViewController: UITableViewController
     /// The blog to manage domains of
     ///
     let blog: Blog
-    
+
+    // MARK: - Properties
+
+    private var contents: [SectionContent]!
+
     // MARK: - Initializer
 
     /// Designated initializer for DomainsViewController
@@ -34,37 +38,120 @@ public class DomainsViewController: UITableViewController
         
         title = NSLocalizedString("Domains", comment: "Title of blog domain management page")
         
+        contents = [.AddDomain]
+        
         WPStyleGuide.resetReadableMarginsForTableView(tableView)
         WPStyleGuide.configureColorsForView(view, andTableView: tableView)
     }
     
     // MARK: - Table View Data Source
     
+    /// Sections in the controller
+    ///
+    private enum SectionContent
+    {
+        case AddDomain
+        
+        var numberOfRows: Int {
+            switch self {
+            case .AddDomain:
+                return AddDomainContent.rows.count
+            }
+        }
+
+        var title: String {
+            switch self {
+            case .AddDomain:
+                return NSLocalizedString("Add A New Domain", comment: "Section title for 'Add A New Domain' in Domains screen")
+            }
+        }
+        
+        func cell(row: Int) -> UITableViewCell {
+            switch self {
+            case .AddDomain:
+                return AddDomainContent.rows[row].cell
+            }
+        }
+    }
+
+    /// Rows in the 'Add A New Domain' section
+    ///
+    private enum AddDomainContent
+    {
+        case FindNew
+        case ConnectOwn
+        
+        static let rows: [AddDomainContent] = [.FindNew, .ConnectOwn]
+        
+        var title: String {
+            switch self {
+            case .FindNew:
+                return NSLocalizedString("Find A New Domain", comment: "Label for 'Find A New Domain' in Domains screen")
+            case .ConnectOwn:
+                return NSLocalizedString("Or connect your own domain", comment: "Label for 'Connect Your Own Domain' in Domains screen")
+            }
+        }
+        
+        var cell: UITableViewCell {
+            let cell = WPTableViewCell(style: .Value1, reuseIdentifier: nil)
+            cell.textLabel?.text = title
+            switch self {
+            case .FindNew:
+                WPStyleGuide.configureTableViewActionCell(cell)
+            case .ConnectOwn:
+                WPStyleGuide.configureTableViewCell(cell)
+            }
+
+            return cell
+        }
+    }
+    
     override public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 0
+        return contents.count
     }
     
     override public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return contents[section].numberOfRows
     }
     
     override public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        return contents[indexPath.section].cell(indexPath.row)
     }
 
     // MARK: - Table View Delegate
     
     override public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        switch contents[indexPath.section] {
+        case .AddDomain:
+            switch AddDomainContent.rows[indexPath.row] {
+            case .FindNew:
+                findNewDomain()
+            case .ConnectOwn:
+                connectOwnDomain()
+                break
+            }
+        }
     }
 
     override public func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        return nil
+        let header  = WPTableViewSectionHeaderFooterView(reuseIdentifier: nil, style: .Header)
+        header.title = contents[section].title
+        
+        return header
     }
 
     override public func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return CGFloat.min
+        return WPTableViewSectionHeaderFooterView.heightForHeader(contents[section].title, width: tableView.frame.width)
     }
 
     // MARK: - Actions
+    
+    private func findNewDomain() {
+        // TODO: Implement findNewDomain()
+    }
 
+    private func connectOwnDomain() {
+        // TODO: Implement connectOwnDomain()
+    }
 }
